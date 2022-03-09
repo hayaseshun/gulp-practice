04d299959f5e20a74a2cbdecdb00052f5a80c898
早瀬　駿 <hayase0217@gmail.com>
2022-03-09 15:43:46 +0900

    no message

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,9 @@
 const gulp = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
+const postcss = require("gulp-postcss");
+const autoprefixer = require("autoprefixer");
+const cssSorter = require("css-declaration-sorter");
+const mmq = require("gulp-merge-media-queries");
 
 function test(done) {
   console.log("hello Gulp");  //処理の内容
@@ -8,9 +12,11 @@ function test(done) {
 
 function compileSass() {
   //元になるフォルダー src
-  return gulp.src("./src/style.scss")
+  return gulp.src("./src/**/*.scss")
   //コンパイルの処理を記載する
   .pipe(sass())
+  .pipe(postcss([autoprefixer(), cssSorter()])) 
+  .pipe(mmq())
   //出力先のフォルダー public
   .pipe(gulp.dest("./public"))//出力先のパス
 }
